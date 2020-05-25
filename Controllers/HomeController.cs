045ceffc89d4f982735ef274e5b0300fe6f997fb
Gabriel McCormack <gmccormack09@qub.ac.uk>
2020-05-25 23:37:16 +0100

    Added Program

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View(new GuestResponse());
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {        
+                // TODO: Email response to the party organizer  
+                return View("Thanks", guestResponse);} 
+            else {
+                // there is a validation error
+                return View(guestResponse);
+            }
+        }
+    }
+}
